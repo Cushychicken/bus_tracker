63ad58bc0a2b70c4d7ec4618bab91f66adafdcd1
Nash Reilly <nrc.reilly@gmail.com>
2014-05-20 07:42:21 -0400

    Tweaks for RPi

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from math import radians, cos, sin, asin, sqrt
 
@@ -28,20 +29,51 @@ def haversine(coord1, coord2):
 # Known locations
 DAVIS_SQUARE = (42.396589, -71.122363)
 UNION_SQUARE = (42.380298, -71.096592)
-DAVIS_TO_UNION = haversine(DAVIS_SQUARE, UNION_SQUARE)
 HOME = (42.387048, -71.114295)
 
-BUS_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=mbta&t=0'
-r = requests.get(BUS_URL)
+distances = {
+    'davis_to_union': haversine(DAVIS_SQUARE, UNION_SQUARE),
+    'davis_to_home' : haversine(DAVIS_SQUARE, HOME),
+    'union_to_home' : haversine(UNION_SQUARE, HOME)
+    }
 
-soup = BeautifulSoup(r.text)
-bus_locations = []
+def bus_locs(dist):
+    DAVIS_SQUARE = (42.396589, -71.122363)
+    UNION_SQUARE = (42.380298, -71.096592)
+    HOME = (42.387048, -71.114295)
 
-for loc in soup.findAll('vehicle', routetag='87'):
-    b = (float(loc.attrs['lat']), float(loc.attrs['lon']))
-    print 'Distance to Davis:', str(haversine(b, DAVIS_SQUARE))
-    print 'Distance to Union:', str(haversine(b, UNION_SQUARE))
-    print 'Distance to Home :', str(haversine(b, HOME))
-    print 'Bus direction    :', loc.attrs['dirtag']
-    print 'Bus ID           :', loc.attrs['id']
-    print
+    BUS_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=mbta&t=0'
+    r = requests.get(BUS_URL)
+
+    status = {
+        'inbound' : False,
+        'outbound': False  
+    }
+
+    soup = BeautifulSoup(r.text)
+    for loc in soup.findAll('vehicle', routetag='87'):
+        b = (float(loc.attrs['lat']), float(loc.attrs['lon']))
+        print 'Distance to Davis:', str(haversine(b, DAVIS_SQUARE))
+        print 'Distance to Union:', str(haversine(b, UNION_SQUARE))
+        print 'Distance to Home :', str(haversine(b, HOME))
+        print 'Bus direction    :', loc.attrs['dirtag']
+        print 'Bus ID           :', loc.attrs['id']
+        print
+
+        if haversine(b, UNION_SQUARE) < dist['union_to_home'] and loc.attrs == '87_0_var1':
+            status['outbound'] = True
+        if haversine(b, DAVIS_SQUARE) < dist['davis_to_home'] and loc.attrs == '87_1_var1':
+            status['inbound'] = True
+
+    return status
+
+        
+if __name__ == '__main__':
+    while True:
+        status = bus_locs(distances)
+        for k,v in status.iteritems():
+            print k,v
+        print
+        time.sleep(60)
+
+
